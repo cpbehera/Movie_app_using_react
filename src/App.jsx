337250d10db77249f828a9dad23e7ae7b340c8d4
Chinmaya Prasad <cpbehera03@gmail.com>
2024-04-08 10:09:19 +0530

    added more movies

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         <>
             <div className='heading_wrapper'>
             { (favSeries === 'netflix') ? 
-            <h1 className='heading'>List of Top 6 Netflix movies of 2023</h1> : 
+            <h1 className='heading'>List of Top 7 Netflix movies of 2023</h1> : 
             <h1 className='heading'>List of Top 6 Amazon Prime movies of 2023</h1> }
             </div>
 
