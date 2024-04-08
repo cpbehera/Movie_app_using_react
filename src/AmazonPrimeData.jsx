9894c266cc4482df8e5b4bf879a651f1f1d2cbf0
Chinmaya Prasad <cpbehera03@gmail.com>
2024-04-08 10:01:17 +0530

    changed movies download link

--- src/AmazonPrimeData.jsx
+++ src/AmazonPrimeData.jsx
@@ -3,13 +3,13 @@ const amazonPrimeData = [
         id : 1,
         imgsrc : 'https://www.scrolldroll.com/wp-content/uploads/2023/08/pathaan-best-hindi-movies-of-2023-on-amazon-prime.jpg',
         title : 'Pathan',
-        link : 'https://downloadhub.tel/watch-online-pathaan-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/watch-online-pathaan-2023-hindi-hdrip-full-movie/'
     },
     {
         id : 2,
         imgsrc : 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQQVr-9Igr6X3D0-jiMKRtmqriCv-LKylJXZ8Uw8Zl_8rts0kcU',
         title : 'Bawaal',
-        link : 'https://downloadhub.tel/bawaal-2023-full-hindi-movie-download-hdrip/'
+        link : 'https://downloadhub.meme/bawaal-2023-full-hindi-movie-download-hdrip/'
     },
     {
         id : 3,
@@ -21,19 +21,19 @@ const amazonPrimeData = [
         id : 4,
         imgsrc : 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRrk_dWSquF52eXrXR5xiGqre4H_Dwo4IYxdWZJz3coGw23a3sR',
         title : 'Rocky Aur Rani Kii Prem Kahaani',
-        link : 'https://downloadhub.tel/rocky-aur-rani-kii-prem-kahaani-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/rocky-aur-rani-kii-prem-kahaani-2023-hindi-hdrip-full-movie/'
     },
     {
         id : 5,
         imgsrc : 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR7fWu7IoeDWvYuJ9DGHqZ06lja0PtrufwW10Ao_n4_A1m5UJ9d',
         title : 'Fukrey 3',
-        link : 'https://downloadhub.tel/fukrey-3-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/fukrey-3-2023-hindi-hdrip-full-movie/'
     },
     {
         id : 6,
         imgsrc : 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdIHqMpE19LQNNu_zU5nRNyIVmdYiEeSdK1Wu2LJNJYR2gHBRx',
         title : 'Satyaprem Ki Katha',
-        link : 'https://downloadhub.tel/satyaprem-ki-katha-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/satyaprem-ki-katha-2023-hindi-hdrip-full-movie/'
     }
 ];
 
--- src/Data.jsx
+++ src/Data.jsx
@@ -3,43 +3,43 @@ const moviesData = [
         id : 1,
         imgsrc : 'https://assets.gadgets360cdn.com/pricee/assets/product/202206/Jawan-poster_1655912386.jpg',
         title : 'Jawan',
-        link : 'https://downloadhub.tips/jawan-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/jawan-2023-hindi-hdrip-full-movie/'
     },
     {
         id : 2,
         imgsrc : 'https://m.media-amazon.com/images/M/MV5BOGZmOTUxYTYtOGExOC00ZjAwLTk5MTItN2Q3ZjIyYTYzY2Y0XkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg',
         title : 'The Archies',
-        link : 'https://downloadhub.tips/the-archies-2023-hindi-dubbed-dual-audio-web-dl-full-movie/'
+        link : 'https://downloadhub.meme/the-archies-2023-hindi-dubbed-dual-audio-web-dl-full-movie/'
     },
     {
         id : 3,
         imgsrc : 'https://m.media-amazon.com/images/M/MV5BNWYzMWFhYmQtNmI0Ni00OWJkLWFiMjktM2JlZmEzZjgzNjE1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
         title : 'Khufiya',
-        link : 'https://downloadhub.tips/khufiya-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/khufiya-2023-hindi-hdrip-full-movie/'
     },
     {
         id : 4,
         imgsrc : 'https://m.media-amazon.com/images/M/MV5BMTNmMmY5NzAtNTdjOC00Y2JiLThjZDQtNDEzODQ5MWQ4NWFhXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
         title : 'Jaane Jaan',
-        link : 'https://downloadhub.tips/jaane-jaan-2023-hindi-hdrip-full-movie/'
+        link : 'https://downloadhub.meme/jaane-jaan-2023-hindi-hdrip-full-movie/'
     },
     {
         id : 5,
         imgsrc : 'https://m.media-amazon.com/images/M/MV5BNjQ5ZmZkNDAtOTc4NC00YzYyLWI1MWItNTNiZmE2NWFlZGIyXkEyXkFqcGdeQXVyMTUzOTcyODA5._V1_.jpg',
         title : 'Lust Stories 2',
-        link : 'https://downloadhub.tips/lust-stories-2-2023-full-movie-hindi-download-hdrip/'
+        link : 'https://downloadhub.meme/lust-stories-2-2023-full-movie-hindi-download-hdrip/'
     },
     {
         id : 6,
         imgsrc : 'https://m.media-amazon.com/images/M/MV5BOGZlNjAzMzgtOTY0Ny00YzdiLWEyNGYtODJlMGM1MTE4ZmMwXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
         title : 'Kathal-A Jackfruit Mystery',
-        link : 'https://downloadhub.tips/kathal-a-jackfruit-mystery-2023-full-hindi-movie-download-hdrip/'
+        link : 'https://downloadhub.meme/kathal-a-jackfruit-mystery-2023-full-hindi-movie-download-hdrip/'
     },
     {
-        id : 6,
-        imgsrc : 'https://m.media-amazon.com/images/M/MV5BOGZlNjAzMzgtOTY0Ny00YzdiLWEyNGYtODJlMGM1MTE4ZmMwXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
-        title : 'Kathal-A Jackfruit Mystery',
-        link : 'https://downloadhub.tips/kathal-a-jackfruit-mystery-2023-full-hindi-movie-download-hdrip/'
+        id : 7,
+        imgsrc : 'https://m.media-amazon.com/images/M/MV5BMmY4NTkxYzYtNWFlYi00NWI2LWE1ZDgtMGJlY2UwZDZkODFkXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg',
+        title : 'Mission Raniganj',
+        link : 'https://downloadhub.meme/mission-raniganj-2023-hindi-hdrip-full-movie/'
     }
 ];
 
